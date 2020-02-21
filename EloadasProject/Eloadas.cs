96d2a9edeb40e264d45537d5e25164a07097c5d7
Kótai Dániel <kotaidani0810@gmail.com>
2020-02-21 11:06:31 +0100

    matrix, feltoltese, lefoglal fuggveny elkeszitve

--- EloadasProject/Eloadas.cs
+++ EloadasProject/Eloadas.cs
@@ -8,6 +8,48 @@ namespace EloadasProject
 {
     public class Eloadas
     {
-
+        bool[,] foglalasok;
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            foglalasok = new bool[sorokSzama, helyekSzama];
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < helyekSzama; j++)
+                {
+                    foglalasok[i, j] = false;
+                }
+            }
+            if (sorokSzama <= 0 || helyekSzama <= 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+        bool Lefoglal()
+        {  
+            for (int i = 0; i < foglalasok.Length-1; i++)
+            {
+                for (int j = 0; j < foglalasok.Length-1; j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        foglalasok[i, j] = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        int SzabadHelyekSzama()
+        {
+            return;
+        }
+        bool Teli()
+        {
+            return;
+        }
+        bool Foglalt(int sorSzam, int helySzam)
+        {
+            return;
+        }
     }
 }
